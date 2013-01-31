d6c2cbbe412cbccb086d05e73d1bb23a4c7c2501
Jordan Orelli <jordanorelli@gmail.com>
2013-01-30 20:55:07 -0500

    removed startserver from base project template
    also, if you're on OS X, a browser will be opened after you create a new
    project.

--- core/config.go
+++ core/config.go
@@ -2,7 +2,9 @@ package din
 
 import (
 	"encoding/json"
+	"io"
 	"os"
+	"path/filepath"
 )
 
 var Config config
@@ -14,15 +16,57 @@ type config struct {
 	} `json:"core"`
 }
 
-func ParseConfigFile(path string) error {
+func (c *config) parseFile(path string) error {
 	fi, err := os.Open(path)
 	if err != nil {
 		return err
 	}
 	defer fi.Close()
 
-	if err := json.NewDecoder(fi).Decode(&Config); err != nil {
+	if err := json.NewDecoder(fi).Decode(c); err != nil {
 		return err
 	}
 	return nil
+}
+
+func (c *config) dump(w io.Writer) error {
+	return json.NewEncoder(w).Encode(c)
+}
+
+func (c *config) dumpToFile(path string, overWrite bool) (err error) {
+	var flag int
+	if overWrite {
+		flag = os.O_CREATE | os.O_RDWR | os.O_TRUNC
+	} else {
+		flag = os.O_CREATE | os.O_WRONLY | os.O_EXCL
+	}
+	fi, err := os.OpenFile(path, flag, 0666)
+	if err != nil {
+		return err
+	}
+	defer fi.Close()
+
+	return c.dump(fi)
+}
+
+func ParseConfigFile(path string) error {
+	return Config.parseFile(path)
+}
+
+// locateConfig looks for the din configuration file.  The configuration file
+// is looked for in the following locations:
+//
+//   - environment variable DIN_CONFIG
+//   - file named config.json in the current directory
+//
+func locateConfig() string {
+	path := os.Getenv("DIN_CONFIG")
+	if path != "" {
+		return path
+	}
+	cwd, err := os.Getwd()
+	if err != nil {
+		return ""
+	}
+	return filepath.Join(cwd, "config.json")
 }
--- core/serve.go
+++ core/serve.go
@@ -1,9 +1,11 @@
 package din
 
 import (
+	"errors"
 	"flag"
 	"fmt"
 	"os"
+	"path/filepath"
 )
 
 func ParseAndRun() {
@@ -23,4 +25,44 @@ func ParseAndRun() {
 		os.Exit(0)
 	}
 	cmdRegistry.run(args)
+}
+
+func parseRoutesFile() (*Router, error) {
+	cwd, err := os.Getwd()
+	if err != nil {
+		return nil, InternalServerError("unable to find routes file")
+	}
+	return ParseRouteFile(filepath.Join(cwd, "routes.json"))
+}
+
+func init() {
+	RegisterCommand(Command{
+		UsageLine: "runserver",
+		Short:     "runs the Din server",
+		Long: `
+This runs the Din webserver.  Blah blah blah this is the long description so
+I should write some more stuff, But I don't really feel like it I'm pretty
+wasted and I really like absinthe, specifically 'Vieux de Pontarlier' is really
+great.
+`,
+		Run: func(cmd *Command, args []string) {
+			router, err := parseRoutesFile()
+			if err != nil {
+				cmd.Bail(err)
+			}
+			path := locateConfig()
+			if path == "" {
+				cmd.Bail(errors.New("unable to locate din config file.  Please set environment variable $DIN_CONFIG to be the absolute path of your configuration file."))
+			}
+			fmt.Println("using config found at " + path)
+			if err := ParseConfigFile(path); err != nil {
+				cmd.Bail(err)
+			}
+			fmt.Println(Config)
+			if err := router.ListenAndServe(Config.Core.Addr); err != nil {
+				os.Stderr.WriteString(err.Error())
+				os.Exit(3)
+			}
+		},
+	})
 }
